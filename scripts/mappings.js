e9d7ae2ef5691a293cacfdee3d24c752e66653aa
Adithya <adithya690@gmail.com>
2025-12-26 18:33:18 +0530

    Optimize maps initialization

--- scripts/mappings.js
+++ scripts/mappings.js
@@ -1,84 +1,82 @@
-export const weatherCategoryCodes = new Map();
-(function fillWeatherCategoryCodes(){
-  weatherCategoryCodes.set(1000, 'sunny');
+export const weatherCategoryCodes = new Map([
+  [1000, 'sunny'],
 
-  weatherCategoryCodes.set(1003, 'cloudy');
-  weatherCategoryCodes.set(1006, 'cloudy');
-  weatherCategoryCodes.set(1009, 'cloudy');
+  [1003, 'cloudy'],
+  [1006, 'cloudy'],
+  [1009, 'cloudy'],
 
-  weatherCategoryCodes.set(1030, 'foggy');
-  weatherCategoryCodes.set(1135, 'foggy');
-  weatherCategoryCodes.set(1147, 'foggy');
+  [1030, 'foggy'],
+  [1135, 'foggy'],
+  [1147, 'foggy'],
 
-  weatherCategoryCodes.set(1063, 'rainy');
-  weatherCategoryCodes.set(1069, 'rainy');
-  weatherCategoryCodes.set(1072, 'rainy');
-  weatherCategoryCodes.set(1150, 'rainy');
-  weatherCategoryCodes.set(1153, 'rainy');
-  weatherCategoryCodes.set(1168, 'rainy');
-  weatherCategoryCodes.set(1171, 'rainy');
-  weatherCategoryCodes.set(1180, 'rainy');
-  weatherCategoryCodes.set(1183, 'rainy');
-  weatherCategoryCodes.set(1186, 'rainy');
-  weatherCategoryCodes.set(1189, 'rainy');
-  weatherCategoryCodes.set(1192, 'rainy');
-  weatherCategoryCodes.set(1195, 'rainy');
-  weatherCategoryCodes.set(1198, 'rainy');
-  weatherCategoryCodes.set(1201, 'rainy');
-  weatherCategoryCodes.set(1204, 'rainy');
-  weatherCategoryCodes.set(1207, 'rainy');
-  weatherCategoryCodes.set(1237, 'rainy');
-  weatherCategoryCodes.set(1240, 'rainy');
-  weatherCategoryCodes.set(1243, 'rainy');
-  weatherCategoryCodes.set(1246, 'rainy');
-  weatherCategoryCodes.set(1249, 'rainy');
-  weatherCategoryCodes.set(1252, 'rainy');
-  weatherCategoryCodes.set(1261, 'rainy');
-  weatherCategoryCodes.set(1264, 'rainy');
+  [1063, 'rainy'],
+  [1069, 'rainy'],
+  [1072, 'rainy'],
+  [1150, 'rainy'],
+  [1153, 'rainy'],
+  [1168, 'rainy'],
+  [1171, 'rainy'],
+  [1180, 'rainy'],
+  [1183, 'rainy'],
+  [1186, 'rainy'],
+  [1189, 'rainy'],
+  [1192, 'rainy'],
+  [1195, 'rainy'],
+  [1198, 'rainy'],
+  [1201, 'rainy'],
+  [1204, 'rainy'],
+  [1207, 'rainy'],
+  [1237, 'rainy'],
+  [1240, 'rainy'],
+  [1243, 'rainy'],
+  [1246, 'rainy'],
+  [1249, 'rainy'],
+  [1252, 'rainy'],
+  [1261, 'rainy'],
+  [1264, 'rainy'],
 
-  weatherCategoryCodes.set(1066, 'snowy');
-  weatherCategoryCodes.set(1114, 'snowy');
-  weatherCategoryCodes.set(1117, 'snowy');
-  weatherCategoryCodes.set(1210, 'snowy');
-  weatherCategoryCodes.set(1213, 'snowy');
-  weatherCategoryCodes.set(1216, 'snowy');
-  weatherCategoryCodes.set(1219, 'snowy');
-  weatherCategoryCodes.set(1222, 'snowy');
-  weatherCategoryCodes.set(1225, 'snowy');
-  weatherCategoryCodes.set(1255, 'snowy');
-  weatherCategoryCodes.set(1258, 'snowy');
+  [1066, 'snowy'],
+  [1114, 'snowy'],
+  [1117, 'snowy'],
+  [1210, 'snowy'],
+  [1213, 'snowy'],
+  [1216, 'snowy'],
+  [1219, 'snowy'],
+  [1222, 'snowy'],
+  [1225, 'snowy'],
+  [1255, 'snowy'],
+  [1258, 'snowy'],
 
-  weatherCategoryCodes.set(1087, 'thunderstorm');
-  weatherCategoryCodes.set(1273, 'thunderstorm');
-  weatherCategoryCodes.set(1276, 'thunderstorm');
-  weatherCategoryCodes.set(1279, 'thunderstorm');
-  weatherCategoryCodes.set(1282, 'thunderstorm');
-})();
+  [1087, 'thunderstorm'],
+  [1273, 'thunderstorm'],
+  [1276, 'thunderstorm'],
+  [1279, 'thunderstorm'],
+  [1282, 'thunderstorm']
+]);
 
-export const hourMappings = new Map();
-(function fillHourMappings(){
-  hourMappings.set(0,  '00:00');
-  hourMappings.set(1,  '01:00');
-  hourMappings.set(2,  '02:00');
-  hourMappings.set(3,  '03:00');
-  hourMappings.set(4,  '04:00');
-  hourMappings.set(5,  '05:00');
-  hourMappings.set(6,  '06:00');
-  hourMappings.set(7,  '07:00');
-  hourMappings.set(8,  '08:00');
-  hourMappings.set(9,  '09:00');
-  hourMappings.set(10, '10:00');
-  hourMappings.set(11, '11:00');
-  hourMappings.set(12, '12:00');
-  hourMappings.set(13, '13:00');
-  hourMappings.set(14, '14:00');
-  hourMappings.set(15, '15:00');
-  hourMappings.set(16, '16:00');
-  hourMappings.set(17, '17:00');
-  hourMappings.set(18, '18:00');
-  hourMappings.set(19, '19:00');
-  hourMappings.set(20, '20:00');
-  hourMappings.set(21, '21:00');
-  hourMappings.set(22, '22:00');
-  hourMappings.set(23, '23:00');
-})();
+export const hourMappings = new Map([
+  [0,  '00:00'],
+  [1,  '01:00'],
+  [2,  '02:00'],
+  [3,  '03:00'],
+  [4,  '04:00'],
+  [5,  '05:00'],
+  [6,  '06:00'],
+  [7,  '07:00'],
+  [8,  '08:00'],
+  [9,  '09:00'],
+  [10, '10:00'],
+  [11, '11:00'],
+  [12, '12:00'],
+  [13, '13:00'],
+  [14, '14:00'],
+  [15, '15:00'],
+  [16, '16:00'],
+  [17, '17:00'],
+  [18, '18:00'],
+  [19, '19:00'],
+  [20, '20:00'],
+  [21, '21:00'],
+  [22, '22:00'],
+  [23, '23:00']
+]);
